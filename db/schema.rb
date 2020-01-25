ef1028eb3d7f81b0c4d1a0f5a75f25953baabd6c
vini-r <vinicius.rodrigues@youse.com.br>
2020-01-25 20:24:54 -0300

    vr-generate_fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_191234) do
+ActiveRecord::Schema.define(version: 2020_01_25_201226) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
     t.string "location"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "password"
+    t.string "location"
+    t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
