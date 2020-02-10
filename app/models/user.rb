05f89eaede72dd03c32fb8e6439e704352de1d17
Vinicius <56924836+vinicius-rodrigues1@users.noreply.github.com>
2020-02-10 15:04:21 -0300

    Creating some notifications for new users on edit page (#2)
    * template
    
    * Creating some notifications for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-    attr_accessible :bio, :email, :full_name, :password
+    attr_accessible :bio, :email, :full_name, :password, :password_confirmation
 
     validates_presence_of :email, :full_name, :password
     validates_confirmation_of :password
     validates_length_of :bio, minimum => 30, :allow_blank => false
+    validates_format_of :email, with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+    validates_uniqueness_of :email
 end
